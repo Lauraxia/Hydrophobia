cfdaebd3e09af6c26233c6b07ed03b59a1f173b5
Alexsci <alexsci@hotmail.com>
2011-12-08 00:50:53 -0500

    Testing Camera upload

---
+++ camera.h
@@ -0,0 +1,29 @@
+#include "common.h"
+
+#ifndef CAMERA
+#define CAMERA
+
+class Camera
+{
+	float postion[3];
+private:
+	float matrix[16];
+public:
+	Camera();
+	Camera(float pos[]);
+	void setView();
+	void move(float pos[], float distance);
+	void setPosition(float pos[]);
+	void rotate(float deg, float refNormal[]);
+	void gRotate(float deg, float ref[]);
+};
+
+/*void camera(float* v);
+void rotate(float x, float y);
+void forward();
+void back();
+void right();
+void left();
+void updateCam();
+*/
+#endif
