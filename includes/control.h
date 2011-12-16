cdc9dd385a5b892824e188c37ba10de24c3889c5
Alexsci <alexsci@hotmail.com>
2011-12-16 00:48:31 -0500

    control header file updated

--- includes/control.h
+++ includes/control.h
@@ -1,9 +1,10 @@
 #include "includes\common.h"
-#include "camera.h"
+#include "includes\camera.h"
 
 void keyUp(unsigned char key, int x, int y);
 void keySpecialUp(int key, int x, int y);
 void kbd(unsigned char key, int x, int y);
 void special(int key, int x, int y);
+void mouse(int button, int state, int x, int y);
 void passiveMouse(int x,int y);
 void handleKeys();
